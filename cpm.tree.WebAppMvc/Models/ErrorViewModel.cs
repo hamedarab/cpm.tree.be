a173df1535d1636326f97cc4f90c25fb189237dd
mh.arab <hamedarab@outlook.com>
2022-05-17 12:06:37 +0430

    deleted first mvc

--- cpm.tree.WebAppMvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace cpm.tree.WebAppMvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
