42a5429a3e1a2a24cc5fbabb50bdf30238839b7b
mh.arab <hamedarab@outlook.com>
2022-05-17 12:45:22 +0430

    2nd try to create asp core mvc app

---
+++ cpm.tree.WebAppMvcIndividualAccount/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cpm.tree.WebAppMvcIndividualAccount.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ cpm.tree.WebAppMvcMsIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cpm.tree.WebAppMvcMsIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
