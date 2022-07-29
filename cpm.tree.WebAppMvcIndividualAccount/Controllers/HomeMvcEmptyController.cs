79123ae447f0aae37d15fb97b6cc31da1e452472
mh.arab <hamedarab@outlook.com>
2022-07-29 09:42:02 +0430

    scaffold test

---
+++ cpm.tree.WebAppMvcIndividualAccount/Controllers/HomeMvcEmptyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cpm.tree.WebAppMvcIndividualAccount.Controllers
+{
+    public class HomeMvcEmptyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ cpm.tree.WebAppMvcIndividualAccount/Controllers/HomeMvcReadWriteController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cpm.tree.WebAppMvcIndividualAccount.Controllers
+{
+    public class HomeMvcReadWriteController : Controller
+    {
+        // GET: HomeMvcReadWriteController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HomeMvcReadWriteController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeMvcReadWriteController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HomeMvcReadWriteController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeMvcReadWriteController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeMvcReadWriteController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeMvcReadWriteController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeMvcReadWriteController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
